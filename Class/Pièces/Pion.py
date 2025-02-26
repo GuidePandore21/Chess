c6035d9c37a61cd00ced1c2430862476e5128ee1
Thomas Lyautey <tlyautey1@gmail.com>
2025-02-26 15:41:11 +0100

    Feat : Création classe Pion.py

---
+++ Class/Pièces/Pion.py
@@ -0,0 +1,17 @@
+from Pièces.Pieces import Piece
+
+class Pion(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.isFirstMove = True
+    
+    def getPossibleMoves(self, position):
+        listeCoups = []
+        if self.color == "blanc":
+            if self.isFirstMove:
+                listeCoups.append((position[0], str(int(position[1]) + 2)))
+            listeCoups.append((position[0], str(int(position[1]) + 1)))
+        else:
+            if self.isFirstMove:
+                listeCoups.append((position[0], str(int(position[1]) - 2)))
+            listeCoups.append((position[0], str(int(position[1]) - 1)))
