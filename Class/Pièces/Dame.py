4230bdbd6e30414f8826cb7ef230ab8697befa61
Thomas Lyautey <tlyautey1@gmail.com>
2025-02-27 15:10:57 +0100

    Refactor : pass la méthode getPossibleMoves dans la classe Dame.py

--- Class/Pièces/Dame.py
+++ Class/Pièces/Dame.py
@@ -5,22 +5,4 @@ class Dame(Piece):
         super().__init__(color)
     
     def getPossibleMoves(self, position):
-        listeCoups = []
-        for i in range(8):
-            listeCoups.append((position[0], self.listeNumeroLignes[i + 1]))
-            listeCoups.append((self.listeLettreColonnes[i + 1], position[1]))
-            
-        for i in range(8):
-            if self.listeLettreColonnes.index(position[0]) + i + 1 < 8 and self.listeNumeroLignes.index(position[1]) + i + 1 < 8:
-                listeCoups.append((self.listeLettreColonnes[self.listeLettreColonnes.index(position[0]) + i + 1], self.listeNumeroLignes[self.listeNumeroLignes.index(position[1]) + i + 1]))
-            
-            if self.listeLettreColonnes.index(position[0]) - i - 1 >= 0 and self.listeNumeroLignes.index(position[1]) - i - 1 >= 0:
-                listeCoups.append((self.listeLettreColonnes[self.listeLettreColonnes.index(position[0]) - i - 1], self.listeNumeroLignes[self.listeNumeroLignes.index(position[1]) - i - 1]))
-            
-            if self.listeLettreColonnes.index(position[0]) + i + 1 < 8 and self.listeNumeroLignes.index(position[1]) - i - 1 >= 0:
-                listeCoups.append((self.listeLettreColonnes[self.listeLettreColonnes.index(position[0]) + i + 1], self.listeNumeroLignes[self.listeNumeroLignes.index(position[1]) - i - 1]))
-           
-            if self.listeLettreColonnes.index(position[0]) - i - 1 >= 0 and self.listeNumeroLignes.index(position[1]) + i + 1 < 8:
-                listeCoups.append((self.listeLettreColonnes[self.listeLettreColonnes.index(position[0]) - i - 1], self.listeNumeroLignes[self.listeNumeroLignes.index(position[1]) + i + 1]))
-        
-        return listeCoups
+        pass
